016d509cf1cf3c98366ba475d1148019ee43c655
sonya <sonyasimonenko20093@gmail.com>
2026-09-11 15:34:47 +0300

    FRONT-6: Resolve conflicts

--- src/frontend/src/components/shared/layout/sidebar.py
+++ src/frontend/src/components/shared/layout/sidebar.py
@@ -9,10 +9,8 @@ def sidebar():
   <nav class="site-sidebar__nav">
     <a class="site-sidebar__link" href="/">Главная</a>
     <a class="site-sidebar__link" href="/generation">Генерация</a>
-    <span class="site-sidebar__link site-sidebar__link--disabled" aria-disabled="true">Галерея</span>
-    <a class="site-sidebar__link" href="/profile">Профиль</a>
     <a class="site-sidebar__link" href="/gallery">Галерея</a>
-    <a class="site-sidebar__link" href="/gallery">Профиль</a>
+    <a class="site-sidebar__link" href="/profile">Профиль</a>
   </nav>
 </aside>
 """
